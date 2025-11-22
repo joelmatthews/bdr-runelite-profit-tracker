f5757d3a70d9d300261442ea400f6f3a81d9e08f
Joel Matthews <joelmatthews.a@gmail.com>
2025-11-22 15:21:19 -0500

    fix

--- src/main/java/com/profittracker/services/leagueservice/LeagueService.java
+++ src/main/java/com/profittracker/services/leagueservice/LeagueService.java
@@ -5,6 +5,6 @@ import java.util.List;
 
 public interface LeagueService {
     public boolean createJsonFile() throws IOException; // creates an empty json file if one does not exist and returns true if successful
-    public boolean addLeaguePlayerId() throws IOException; // adds a league player id to the json file and returns true if successful
+    public boolean addLeaguePlayerId(String id) throws IOException; // adds a league player id to the json file and returns true if successful
     public List<String> getLeaguePlayerIds() throws IOException; // returns a complete list of every league player id in the json file
 }
